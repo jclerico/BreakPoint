ac30de4e54979aa4d16ae9e946ae9d7ea34ae503
Jeremy Clerico <blops2hd@gmail.com>
2017-08-10 18:19:07 +0100

    Created UIView Extension for Binding Views to Keyboard

--- BreakPoint/Controller/CreatePostVC.swift
+++ BreakPoint/Controller/CreatePostVC.swift
@@ -21,9 +21,7 @@ class CreatePostVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.delegate = self
-        
-        
-        
+        sendBtn.bindToKeyboard()
         
     }
     
