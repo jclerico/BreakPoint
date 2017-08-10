220e1a4da769523e384814a4c9b1d9b7fc01fd2c
Jeremy Clerico <blops2hd@gmail.com>
2017-08-10 12:17:32 +0100

    Started setting up DataService and func to create FireBase users

---
+++ BreakPoint/Services/DataService.swift
@@ -0,0 +1,52 @@
+//
+//  DataService.swift
+//  BreakPoint
+//
+//  Created by Jeremy Clerico on 10/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUP = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    var REF_GROUP: DatabaseReference {
+        return _REF_GROUP
+    }
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED
+    }
+    
+    func createDBUser(uid: String, userData: Dictionary<String, Any>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
