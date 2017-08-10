b6683ece12bca9e90ad4c1593aa39c054e5182c8
Jeremy Clerico <blops2hd@gmail.com>
2017-08-10 17:50:32 +0100

    Posts now upload to Firebase and started Creating CreatePostVC

---
+++ BreakPoint/Controller/CreatePostVC.swift
@@ -0,0 +1,67 @@
+//
+//  CreatePostVC.swift
+//  BreakPoint
+//
+//  Created by Jeremy Clerico on 10/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+        
+        
+        
+        
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Say something here" {
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = true
+                    print("There was an error!")
+                }
+            })
+        }
+        
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
+
+
+
+
+
+
+
+
+
+
