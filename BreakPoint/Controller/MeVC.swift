d942152fb5b9bb1ffd33665c163d00531d1f5884
Jeremy Clerico <blops2hd@gmail.com>
2017-08-10 17:20:24 +0100

    Started building MeVC and added to UITabBarController

---
+++ BreakPoint/Controller/MeVC.swift
@@ -0,0 +1,31 @@
+//
+//  MeVC.swift
+//  BreakPoint
+//
+//  Created by Jeremy Clerico on 10/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+    
+    
+}
