2b38afff38c93a4ce72677b92bb52164ab5119c5
Jeremy Clerico <blops2hd@gmail.com>
2017-08-11 15:44:34 +0100

    Created CreateGroupsVC and connected Outlets/Actions

---
+++ BreakPoint/Controller/CreateGroupsVC.swift
@@ -0,0 +1,44 @@
+//
+//  CreateGroupsVC.swift
+//  BreakPoint
+//
+//  Created by Jeremy Clerico on 11/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var titleTextField: InsetTextField!
+    @IBOutlet weak var descriptionTextField: InsetTextField!
+    @IBOutlet weak var emailSearchTextField: InsetTextField!
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func doneBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+}
