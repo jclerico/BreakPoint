b875961fe9a4ddf7ba80e631d1af222cd1679ae6
Jeremy Clerico <blops2hd@gmail.com>
2017-08-11 19:15:24 +0100

    Started creating GroupCell

--- BreakPoint/Controller/GroupsVC.swift
+++ BreakPoint/Controller/GroupsVC.swift
@@ -11,16 +11,31 @@ import UIKit
 class GroupsVC: UIViewController {
     
     
+    @IBOutlet weak var groupsTableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        groupsTableView.delegate = self
+        groupsTableView.dataSource = self
         
         
     }
+}
+
+extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = groupsTableView.dequeueReusableCell(withIdentifier: "groupCell", for: indexPath) as? GroupCell else { return UITableViewCell() }
+        cell.configureCell(title: "A Title", description: "A Description", memberCount: 3)
+        return cell
+    }
 }
 
---
+++ BreakPoint/View/GroupCell.swift
@@ -0,0 +1,25 @@
+//
+//  GroupCell.swift
+//  BreakPoint
+//
+//  Created by Jeremy Clerico on 11/08/2017.
+//  Copyright © 2017 Jeremy Clerico. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+    
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var groupDescLbl: UILabel!
+    @IBOutlet weak var memberCountLbl: UILabel!
+    
+    func configureCell(title: String, description: String, memberCount: Int) {
+        self.groupTitleLbl.text = title
+        self.groupDescLbl.text = description
+        self.memberCountLbl.text = "\(memberCount) members."
+    }
+    
+    
+    
+}
